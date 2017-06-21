77174f97501a9a7516caedb14ac2fecdcfe1e1d1
Marcus Oliveira <killertiger@gmail.com>
2017-06-21 08:11:41 -0300

    Question type

--- questiondb/models.py
+++ questiondb/models.py
@@ -2,14 +2,30 @@ from django.db import models
 
 
 # Create your models here.
-# TODO: explanation, tags, source, shared_text, imagedb, classification
+# TODO: explanation, tags, source, shared_text, imagedb, classification, question type
+from django.utils.html import strip_tags
+
+
 class Question(models.Model):
     title = models.CharField(max_length=300)
     body_html = models.TextField(verbose_name='Text')
     body_plain_text = models.TextField()
 
+    QUESTION_TYPE = (
+        (('t', 'text'),
+         ('m', 'multiple choice')),
+    )
+
+    question_type = models.CharField(max_length=1, choices=QUESTION_TYPE, blank=False, default='t')
+
     def __str__(self):
         return self.title
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+
+        self.body_plain_text = strip_tags(self.body_html)
+        super(Question, self).save()
 
 
 class Alternative(models.Model):
